c8ffc510631f3dd4e83a060285188ab3d1597302
keyboardshortcake <keyboardshortcake@gmail.com>
2022-11-14 05:48:13 -0600

    added basic test

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,24 @@
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+// create a test suite for testing the calculator service
+describe('CalculatorService', () => {
+
+    it('should add two numbers', () => {
+        const calculator = new CalculatorService(new LoggerService)
+
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4)
+    })
+
+    it('should subtract two numbers', () => {
+
+        const calculator = new CalculatorService(new LoggerService)
+
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, "unexpected subtraction result")
+    })
+
+})
